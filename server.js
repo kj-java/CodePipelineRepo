2d788bb93ebf6f586ab3f18a253646f031b41d56
kj-java <10koji25@gmail.com>
2021-04-29 22:25:48 +0900

    docker

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+
+const index = './index.html';
+
+const app = express();
+app.use(express.static('./'));
+app.get('/', (req, res) => {
+  res.sendFile(index);
+});
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+app.listen(PORT, HOST);
+
+console.log(`Running on http://${HOST}:${PORT}`);
